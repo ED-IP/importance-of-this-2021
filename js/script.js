e74d797e1193aca741a8b04458017b9019c3bb85
ED-IP <edip_courses@outlook.es>
2021-04-14 10:27:54 +0000

    remove the code and the p elements/Added code and elements for buttons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
-$("p").click(function(){
-    $(this).slideToggle("slow");
+$("button").mouseenter(function(){
+    $(this).removeClass("makeRed").addClass("makeBlue");
+});
+$("button").mouseleave(function(){
+    $(this).removeClass("makeBlue").addClass("makeRed");
 });
